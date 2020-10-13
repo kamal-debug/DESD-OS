180d653d2893ab69202c6e688fbc0b87fcef5f9c
KamalKantSaini <kamalkant.1812.ks@gmail.com>
2020-10-13 08:06:17 -0700

    demo for make file

---
+++ buildprocess/builddemo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int add(int,int);
+int sub(int,int);
+
+int main(int argc, char const *argv[])
+{
+    printf("Addition:%d\n",add(20,20));
+    printf("Difference:%d\n",sub(30,20));
+    return 0;
+}
