43a8fa769b6cfa31b29fb2bffd7897dca656b0be
KamalKantSaini <kamalkant.1812.ks@gmail.com>
2020-10-16 09:04:20 -0700

    New Friend creating main using existing Library

---
+++ arith/noida/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "arith.h"
+
+
+int main(int argc, char const *argv[])
+{
+    printf("Noida Friend Addition:%d\n",add(30,40));
+    printf("Noida Friend Subtraction:%d\n",sub(50,10));
+    return 0;
+}
