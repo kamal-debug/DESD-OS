289dead7b4221b628e099943bb38ad68b38b195f
KamalKantSaini <kamalkant.1812.ks@gmail.com>
2020-11-02 20:37:48 -0800

    zombie Process

---
+++ zombie/zombie.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    pid_t id;
+
+    id = fork();
+
+    if(id==0){
+
+        printf("Child Starting:\n");
+        sleep(2);
+        printf("child exiting\n");
+    }
+    else
+    {
+        printf("parent Starting:\n");
+        sleep(12);
+        printf("Parent exiting\n");
+    }
+    
+    
+    return 0;
+}
